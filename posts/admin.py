0dee46e1fdbec80794cc87d7601ca77a1ebc80fa
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-06 02:35:34 +0100

    migrated the database, created super user, created the Post model and configured the urls and templates

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Post
 # Register your models here.
+
+admin.site.register(Post)
+
+
+
+# Admin site login details
+# username: mbadmin
+# email: mbadmin@gmail.com
+# password: mbadmin
